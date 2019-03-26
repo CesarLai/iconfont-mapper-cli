d3ec0c74a970319bab973a7b7838d55e0867c263
CesarLai <core@jfdev.cn>
2019-03-26 14:33:33 +0800

    修改README.md

---
+++ README.md
@@ -0,0 +1,47 @@
+# Iconfont Mapper CLI
+一个Iconfont图标映射文件生成工具。
+
+## 介绍
+Iconfont平台是目前深受前端工程师喜爱的一个矢量图标管理平台，在上面可以轻松获取和管理图标。在移动端使用Iconfont的图标时，经常需要json格式的图标映射文件，而从Iconfont下载的字体文件中，并不包含这样的文件。因此，这个工具的作用就是通过读取Iconfont字体压缩文件中的css文件内容，生成我们需要的图标映射文件。
+
+iconfont.css
+```
+.icon-select:before {
+  content: "\e622";
+}
+
+.icon-user:before {
+  content: "\e624";
+}
+
+.icon-switch:before {
+  content: "\e625";
+}
+
+.icon-search:before {
+  content: "\e626";
+}
+```
+
+生成的图标映射文件：iconfont.json
+```
+{
+    "icon-select": 58914,
+    "icon-user": 58916,
+    "icon-switch": 58917,
+    "icon-search": 58918
+}
+```
+
+## 快速上手
+建议在Node.js v8.x或以上版本使用此命令行工具。
+```
+# 安装命令行工具
+$ npm install -g iconfont-mapper-cli
+
+# 使用当前目录的css文件生成json文件
+$ iconfont-mapper -f iconfont.css
+
+# 使用指定目录的css文件生成json文件
+$ iconfont-mapper -f C:\Users\Administrator\Downloads\iconfont.css
+```
---
+++ bin/cli.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var os = require('os');
+var yargs = require('yargs');
+
+// 文件输出目录
+var OUTPUT_FILE_DIR = path.join(os.homedir(), 'Downloads');
+
+// 设置命令参数
+var argv = yargs
+    .usage('Usage: font-mapper [options]')
+    .options({
+        'help': {
+            alias: 'h',
+            describe: '输出帮助信息',
+        },
+        'version': {
+            alias: 'v',
+            describe: '输出版本信息'
+        },
+        'file': {
+            alias: 'f',
+            describe: '根据包含图标样式的css文件生成包含所有图标映射信息的json文件',
+            nargs: 1,
+            type: 'string'
+        }
+    })
+    .argv;
+
+argv.f && fontMapper(argv.f);
+
+/**
+ * 
+ * @param {string} filePath 
+ */
+function fontMapper(filePath) {
+    if (path.extname(filePath) !== '.css') {
+        console.log('文件格式不正确，请输入css文件的路径。');
+        return;
+    }
+
+    // 记录css文件名，用于为输出的json文件命名
+    var baseName = path.basename(filePath, '.css') + '.json';
+    fs.readFile(filePath, 'utf8', (error, content) => {
+        if (error) {
+            console.log(error.message);
+            return;
+        };
+
+        // 匹配单个图标css样式文本的正则表达式
+        var regex = /\.(icon-[a-z-]+)(:\w+)?\s*{\s*\w+:\s*\"(\\\w{4})\";\s*}/g;
+        var results = content.match(regex);
+        var jsonObject = {};
+
+        if (results && results.length > 0) {
+            for (var item of results) {
+                // 匹配图标名和图标对应的16进制数，并保存到用于生成json字符串的对象中
+                var iconNameRegex = /icon-[a-z-]+/g,
+                    iconValueRegex = /\\\w{4}/g;
+                var iconNames = item.match(iconNameRegex),
+                    iconValues = item.match(iconValueRegex);
+
+                // 图标名和图标对应的16进制值都匹配成功
+                if (iconNames && iconNames.length > 0 && iconValues && iconValues.length > 0) {
+                    jsonObject[iconNames[0]] = parseInt(iconValues[0].substring(1), 16);
+                }
+            }
+
+            // 输出json文件
+            var outputFileFullPath = path.join(OUTPUT_FILE_DIR, baseName);
+            fs.writeFile(outputFileFullPath, JSON.stringify(jsonObject), 'utf8', (error) => {
+                if (error) {
+                    console.log(`json文件生成失败，原因：${error.message}。`);
+                } else {
+                    console.log(`json文件生成成功：${outputFileFullPath}`);
+                }
+            });
+        } else {
+            console.log('未匹配到任何图标。');
+        }
+    });
+}
